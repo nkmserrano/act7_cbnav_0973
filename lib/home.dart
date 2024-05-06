296dde369b3ad23804019ec8dfd37f9d640ab494
NAIROBI_KEREN_MONTIEL_SERRANO <a.21308051280973@cbtis128.edu.mx>
2024-05-06 16:00:50 +0000

    Actualizando

---
+++ lib/home.dart
@@ -0,0 +1,51 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _page = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.transparent,
+        buttonBackgroundColor: Colors.cyan,
+        color: Colors.cyan,
+        animationDuration: const Duration(milliseconds: 300),
+        items: const <Widget>[
+          Icon(Icons.home, size: 26, color: Colors.white),
+          Icon(Icons.favorite, size: 26, color: Colors.white),
+          Icon(Icons.add, size: 26, color: Colors.white),
+          Icon(Icons.calendar_month_rounded, size: 26, color: Colors.white),
+          Icon(Icons.person, size: 26, color: Colors.white),
+        ],
+        onTap: (index) {
+          setState(() {
+            _page = index;
+          });
+        },
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+        title: const Text('Nairobi Montiel 0973',
+            style: TextStyle(color: Colors.white)),
+      ),
+      body: Center(
+        child: Text(
+          _page.toString(),
+          style: const TextStyle(
+            fontSize: 100,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
